ce85b397f62aa96630f09580f9c61e0c22282097
Damir-Tugushev <0damir.1tugushev@gmail.com>
2021-09-23 12:59:25 +0300

    Small rewriting

--- app/src/main/java/io/github/damirtugushev/restaurantmanager/MainActivity.kt
+++ app/src/main/java/io/github/damirtugushev/restaurantmanager/MainActivity.kt
@@ -1,7 +1,7 @@
 package io.github.damirtugushev.restaurantmanager
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
